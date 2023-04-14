1362214e667f006ade4894faeca045099a4c58ef
Victor Manuel Osorio Garcia <victormanuelosoriog@gmail.com>
2023-04-14 16:20:05 -0500

    Correcion tamaños banner

--- components/BannerHeader.tsx
+++ components/BannerHeader.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 
 const BannerHeader = () => (
-  <div className='hidden w-screen h-80 flex-col lg:flex'>
+  <div className='hidden debug w-screen h-80 flex-col items-center lg:flex'>
     <div className=' mb-[-35px]'>
       <Image
         src='/Novedades-1.png'
         alt='Banner Header'
-        width={1024}
+        width={1600}
         height={320}
-        className='w-screen h-80'
+        className=' max-w-[1600px] max-h-80'
       />
     </div>
     <div>
