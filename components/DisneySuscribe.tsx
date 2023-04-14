5a91eaf244b77e06f9224a8c180b32f3be1f65fc
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-04-14 15:45:38 -0500

    Inicialización de la sección
    Se pusieron algunos elementos para posteriormente hacer el componente en su totalidad

---
+++ components/DisneySuscribe.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const DisneySuscribe = () => {
+  return (
+    <div>
+      <div className='border'>
+        <span>Aqui es la cabeza</span> 
+      </div>
+      <div className='border'>
+        <span>Consigue los mejores beneficios de Mercado Libre</span>
+      </div>
+      <div className='border'>
+        <span>Disney</span>
+        <span>Star+</span>
+        <span>Envios</span>
+      </div>
+      <div className='border'>
+        <button>Suscribete</button> 
+      </div>
+    </div>
+  )
+}
+
+export default DisneySuscribe
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import DisneySuscribe from '@components/DisneySuscribe';
 import { NextPage } from 'next';
 import Head from 'next/head';
 
@@ -13,6 +14,7 @@ const Home: NextPage = () => (
       <link rel='icon' href='/favicon.svg' />
     </Head>
     <div className='text-3xl font-bold underline'>Proyecto 2</div>
+    <DisneySuscribe></DisneySuscribe>
   </>
 );
 
