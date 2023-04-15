be724776647e2de0b3491612e8de463e22000a59
Victor Manuel Osorio Garcia <victormanuelosoriog@gmail.com>
2023-04-15 12:47:23 -0500

    Categorias

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,19 +1,98 @@
 import Image from 'next/image';
 
+const categoriesInfo = [
+  {
+    image: '/categories/Carros.svg',
+    alt: 'Carros',
+    description: 'Carros, Motos y Otros',
+  },
+  {
+    image: '/categories/Computacion.svg',
+    alt: 'Computación',
+    description: 'Computación',
+  },
+  {
+    image: '/categories/Ropa.svg',
+    alt: 'Ropa',
+    description: 'Ropa y Accesorios',
+  },
+  {
+    image: '/categories/Hogar.svg',
+    alt: 'Hogar',
+    description: 'Hogar y Muebles',
+  },
+  {
+    image: '/categories/Accesorios.svg',
+    alt: 'Accesorios',
+    description: 'Accesorios para Vehículos',
+  },
+  {
+    image: '/categories/Electrodomesticos.svg',
+    alt: 'Electrodomésticos',
+    description: 'Electrodomésticos',
+  },
+  {
+    image: '/categories/Camaras.svg',
+    alt: 'Cámaras',
+    description: 'Cámaras y Accesorios',
+  },
+  {
+    image: '/categories/Celulares.svg',
+    alt: 'Celulares',
+    description: 'Celulares y Teléfonos',
+  },
+  {
+    image: '/categories/Belleza.svg',
+    alt: 'Belleza',
+    description: 'Belleza y Cuidado Personal',
+  },
+  {
+    image: '/categories/Deportes.svg',
+    alt: 'Deportes',
+    description: 'Deportes y Fitness',
+  },
+  {
+    image: '/categories/Electronica.svg',
+    alt: 'Electrónica',
+    description: 'Electrónica, Audio y Video',
+  },
+  {
+    image: '/categories/Consolas.svg',
+    alt: 'Consolas',
+    description: 'Consolas y Videojuegos',
+  },
+  {
+    image: '/categories/Juegos.svg',
+    alt: 'Juegos',
+    description: 'Juegos y Juguetes',
+  },
+  {
+    image: '/categories/Herramientas.svg',
+    alt: 'Herramientas',
+    description: 'Herramientas',
+  },
+];
+
 const Categories = () => {
   {
   }
   return (
     <section className='debug flex w-screen justify-center items-center'>
-      <Categorie description='Carros, motos y otros'>
-        <Image
-          src='/Categorie-Cars.svg'
-          alt='Carros'
-          width={45}
-          height={40}
-          className=''
-        />
-      </Categorie>
+      <div className='grid grid-cols-2 gap-0.5 lg:grid-cols-7'>
+        {categoriesInfo.map((category) => (
+          <Categorie
+            key={`category_${category.alt}`}
+            description={category.description}
+          >
+            <Image
+              src={category.image}
+              alt={category.alt}
+              width={45}
+              height={40}
+            />
+          </Categorie>
+        ))}
+      </div>
     </section>
   );
 };
