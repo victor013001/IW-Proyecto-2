9dd0faabee8553424e66194e403a0a868071ab7f
Victor Manuel Osorio Garcia <victormanuelosoriog@gmail.com>
2023-04-15 18:04:38 -0500

    Section categories finalizada

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -77,21 +77,26 @@ const Categories = () => {
   {
   }
   return (
-    <section className='debug flex w-screen justify-center items-center'>
-      <div className='grid grid-cols-2 gap-0.5 lg:grid-cols-7'>
-        {categoriesInfo.map((category) => (
-          <Categorie
-            key={`category_${category.alt}`}
-            description={category.description}
-          >
-            <Image
-              src={category.image}
-              alt={category.alt}
-              width={45}
-              height={40}
-            />
-          </Categorie>
-        ))}
+    <section className='flex w-screen justify-center items-center'>
+      <div className='flex flex-col gap-5'>
+        <span className='text-base font-semibold lg:text-2xl lg:font-normal text-ml-black-light'>
+          Categorías populares
+        </span>
+        <div className='grid grid-cols-2 gap-0.5 lg:grid-cols-7'>
+          {categoriesInfo.map((category) => (
+            <Categorie
+              key={`category_${category.alt}`}
+              description={category.description}
+            >
+              <Image
+                src={category.image}
+                alt={category.alt}
+                width={45}
+                height={40}
+              />
+            </Categorie>
+          ))}
+        </div>
       </div>
     </section>
   );
@@ -103,8 +108,8 @@ interface CategorieProps {
 }
 
 const Categorie = ({ children, description }: CategorieProps) => (
-  <a className='flex flex-col h-[150px] w-[150px] bg-white items-center justify-center gap-3.5 hover:bg-blue-200 cursor-pointer '>
-    {children}
+  <a className='flex flex-col h-[160px] w-[140px] bg-white items-center justify-center gap-3 hover:bg-blue-200 cursor-pointer '>
+    <div className='flex items-center justify-center'>{children}</div>
     <span className='text-sm text-black text-center'>{description}</span>
   </a>
 );
