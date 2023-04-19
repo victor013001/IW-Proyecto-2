7a2b964eaa96e6d758e7e4392591eefec5c7ee12
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-04-19 14:48:21 -0500

    Inicio de la Sección de ofertas

---
+++ components/oferta.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const oferta = () => {
+  return (
+    <div>
+      <span>imagen</span>
+      <span>precio</span>
+      <span>oferta</span>
+      <span>texto verde</span>
+    </div>
+  )
+}
+
+export {oferta}
