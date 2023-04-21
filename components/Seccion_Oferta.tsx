2c9548834023ab42da0cbaf973c33ec75ff8bbc5
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-04-21 13:49:51 -0500

    Organización sección ofertas

--- components/Seccion_Oferta.tsx
+++ components/Seccion_Oferta.tsx
@@ -1,19 +1,25 @@
 import {Oferta} from '@components/oferta'
+import { Button } from '@mui/material'
 
 const Seccion_Oferta = () => {
+  
+  const toRight = '>'
+  const toLeft = '<'
+  
   return (
     <section className='flex justify-center items-center mb-14'>
-        <div className='w-[1180px] h-[420px] justify-between '>
+        <div className='w-[996px] h-[420px] justify-between '>
             <div className='mb-4 mt-6'>
                 <span className='font-light text-2xl mr-4 text-ml-gray'>Ofertas</span>
                 <span className='text-ml-blue'>Ver todas</span>
             </div>
-            <div className='flex flex-row gap-3'>
+            <div className='flex flex-row gap-3 items-center'>
                 <Oferta imagen='./media/silla_oferta.png' precio='210.000' descuento='32% OFF' promo='Envío gratis'></Oferta>
                 <Oferta imagen='./media/micro_oferta.png'precio='74.990' descuento='25% OFF' promo='Envío gratis Full'></Oferta>
                 <Oferta imagen='./media/afeitadora_oferta.png' precio='134.950' descuento='50% OFF' promo='Envío gratis'></Oferta>
                 <Oferta imagen='./media/armario_oferta.png' precio='149.900' descuento='30% OFF' promo='Envío gratis'></Oferta>
                 <Oferta imagen='./media/cortina_oferta.png' precio='69.990' descuento='17% OFF' promo='Full'></Oferta>
+    
             </div>
         </div>
     </section>
--- components/oferta.tsx
+++ components/oferta.tsx
@@ -8,12 +8,12 @@ interface OfertaProps {
 
 const Oferta = ({imagen, precio, descuento, promo} : OfertaProps) => {
   return (
-    <div className='flex flex-col w-[224px] h-[350px] rounded bg-white'>
-      <div className='border w-[224px] h-[224px]'>
+    <div className='absoluteflex flex-col w-[190px] h-[350px] rounded bg-white'>
+      <div className='border w-[190px] h-[224px]'>
         <img src={imagen}/>
       </div>
       <div className='p-5'>
-        <span className='mr-3 text-2xl font-normal'>${precio}</span>
+        <span className='mr-3 text-xl font-normal'>${precio}</span>
         <span className='text-sm text-ml-green'>{descuento}</span>
         <div>
           <span className='text-sm font-bold text-ml-green'>{promo}</span>
