440b0905a314c0b808a4354169f87903b32c49f5
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-04-22 13:31:44 -0500

    Organización de tamaños para el carrusel

--- components/MyCarousel.jsx
+++ components/MyCarousel.jsx
@@ -3,13 +3,17 @@ import { Oferta } from './oferta'
 import 'react-multi-carousel/lib/styles.css'
 
 const responsive = {
+    superLargeDesktop: {
+        breakpoint: { max:3000, min:1024},
+        items: 5
+      },
     desktop: {
-      breakpoint: { max: 3000, min: 1024},
-      items: 5
+      breakpoint: { max: 1024, min: 768},
+      items: 3
     },
     tablet: {
-      breakpoint: { max:1024, min:640},
-      items: 3
+      breakpoint: { max:768, min:425},
+      items: 2
     },
     mobile: {
       breakpoint: { max:425, min: 0 },
@@ -21,7 +25,7 @@ const MyCarousel = () => {
   return (
         <section className='flex justify-center items-center'>
             <div className='w-[224px] md:w-[996px]'>
-            <Carousel responsive={responsive} removeArrowOnDeviceType={["tablet", "mobile"]}>
+            <Carousel responsive={responsive}>
                 <Oferta imagen='./media/silla_oferta.png' precio='210.000' descuento='32' promo='Envío gratis'></Oferta>
                 <Oferta imagen='./media/micro_oferta.png' precio='74.990' descuento='25' promo='Envío gratis FULL'></Oferta>
                 <Oferta imagen='./media/afeitadora_oferta.png' precio='134.950' descuento='50' promo='Envío gratis'></Oferta>
